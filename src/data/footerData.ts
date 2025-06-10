ed1ac9250d5de33dcd8af8b5a594f52c2817f4d1
Ivan Juan Mendoza Gil <48997774+ivmendozadev@users.noreply.github.com>
2025-06-09 20:18:26 -0600

    footer changes

--- src/data/footerData.ts
+++ src/data/footerData.ts
@@ -3,20 +3,20 @@ export const footerData = {
   socialLinks: [
     {
       name: 'GitHub',
-      url: 'https://github.com/johndoe',
+      url: 'https://github.com/ivmendozadev',
       icon: 'Github'
     },
     {
       name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/johndoe/',
+      url: 'https://www.linkedin.com/in/ivmendozagil/',
       icon: 'Linkedin'
     },
     {
       name: 'Email',
-      url: 'mailto:johndoe@example.com',
+      url: 'mailto:mendozaivangil1@gmial.com',
       icon: 'Mail'
     }
   ],
-  quote: '"Code is like humor. When you have to explain it, it’s bad."',
-  githubCodeLink: 'https://github.com/raulrls/Astro-Shadcn-portfolio'
+  quote: '"El error siempre está en el último lugar donde buscas... porque dejas de buscar cuando lo encuentras."',
+  //githubCodeLink: 'https://github.com/raulrls/Astro-Shadcn-portfolio'
 };
