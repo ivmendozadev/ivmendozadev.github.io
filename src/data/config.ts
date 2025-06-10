cf32388cd11456323aeb732217b3e8b2b2f83831
Ivan Juan Mendoza Gil <48997774+ivmendozadev@users.noreply.github.com>
2025-06-09 19:11:05 -0600

    config file changes

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,12 +1,12 @@
 export const config = {
-  siteTitle: 'John Doe Portfolio',
-  description: 'Hi, I am John Doe, a passionate web developer and data analyst. I thrive on solving problems and building innovative solutions with modern technology.',
-  author: 'John Doe',
-  contactEmail: 'johndoe@example.com',
-  keywords: 'portfolio, business intelligence, data analysis, Power BI Reports, Software Engineer, Web Developer',
+  siteTitle: 'Ivan Mendoza Portafolio',
+  description: 'Hi, I am Ivan Mendoza, a passionate developer and data analyst. I thrive on solving problems and building innovative solutions with modern technology.',
+  author: 'Ivan Mendoza',
+  contactEmail: 'mendozaivangil1@gmail.com',
+  keywords: 'portfolio, business intelligence, data analysis, Software Engineer, Web Developer',
   favicon: '/favicon.svg',
   social: {
-    linkedin: 'https://www.linkedin.com/in/johndoe/',
-    github: 'https://github.com/johndoe'
+    linkedin: 'https://www.linkedin.com/in/ivmendozagil/',
+    github: 'https://github.com/ivmendozadev'
   }
 };
