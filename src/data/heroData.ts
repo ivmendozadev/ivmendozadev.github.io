368460fc7ad8db8d44c8a8f9767bd46eaa56e944
Ivan Juan Mendoza Gil <48997774+ivmendozadev@users.noreply.github.com>
2025-06-09 19:28:15 -0600

    changes profile picture

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -2,7 +2,7 @@ export const heroData = {
   name: 'Ivan Juan Mendoza Gil',
   title: 'ETL | Developer | Data Analyst',
   description: 'I build scalable web applications and transform raw data into meaningful insights that drive business success.',
-  profilePicture: 'https://drive.google.com/file/d/1LJlzV6D3gto2tLCYGOtZ_DwRfbwU-x3L/view?usp=sharing',
+  profilePicture: 'https://drive.google.com/file/d/1w2hu5sQvQrUBcI9KFyEmFcdu9nL2EWx8/view?usp=sharing',
   skills: [
     'SQL',
     'ETL',
